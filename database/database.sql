156c18681f29c61f1be2f78b6c185804384055c3
J. Nick Baughan <JNickBaughan@Gmail.com>
2023-04-30 19:56:23 -0400

    update script

--- database/database.sql
+++ database/database.sql
@@ -1,28 +1,36 @@
-CREATE TABLE AccountType {
+CREATE DATABASE transactions;
+
+CREATE TABLE AccountType (
     AccountTypeID INT IDENTITY(1,1) PRIMARY KEY,
     AccountTypeName VARCHAR(150)
-}
+)
 
-CREATE TABLE Account {
+INSERT INTO AccountType (AccountTypeName)
+VALUES ('cash'), ('credit card'), ('checking'), ('saving');
+
+CREATE TABLE Account (
     AccountID INT IDENTITY(1,1) PRIMARY KEY,
     AccountName VARCHAR(150),
-    AccountType INT FOREIGN KEY REFERENCES AccountType(AccountTypeID)
+    AccountType INT FOREIGN KEY REFERENCES AccountType(AccountTypeID),
     AccountNumber VARCHAR(150),
     AccountDescription VArchar(250)
-}
+)
 
-CREATE TABLE TransactionType {
+CREATE TABLE TransactionType (
     TransactionTypeID INT IDENTITY(1,1) PRIMARY KEY,
     TransactionName VARCHAR(150)
-}
+)
 
-CREATE TABLE Transaction {
+INSERT INTO TransactionType (TransactionName)
+VALUES ('debit'), ('credit');
+
+CREATE TABLE TransactionTable (
     TransactionID INT IDENTITY(1,1) PRIMARY KEY,
     TransactionDate DATETIME NOT NULL,
-    Account INT FOREIGN KEY REFERENCES Account(AccountID)
-    TransactionType INT FOREIGN KEY REFERENCES TransactionType(TransactionID)
+    Account INT FOREIGN KEY REFERENCES Account(AccountID),
+    TransactionType INT FOREIGN KEY REFERENCES TransactionType(TransactionTypeID),
     Vendor VARCHAR(150),
     TransactionDescription VARCHAR(150),
     Comment VARCHAR(255)
     -- TODO add category
-}
+)
